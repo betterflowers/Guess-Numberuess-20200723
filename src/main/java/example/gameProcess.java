f30a770846bb67cfb5f6d4bb449b64a929e33ce2
betterflowers <1054952496@qq.com>
2020-07-24 00:06:22 +0800

    [karol] feature:InputCount

---
+++ src/main/java/example/gameProcess.java
@@ -0,0 +1,13 @@
+package example;
+
+public class gameProcess {
+
+    public boolean inputCount(int countInput){
+        if(countInput <6){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
